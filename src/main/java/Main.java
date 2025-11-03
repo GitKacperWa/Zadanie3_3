cdf5e2c249df6a4fd63514707573b8a486c06479
GitKacperWa <kwalas@onet.pl>
2025-11-03 09:07:31 +0000

    commit-3_2.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,22 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.Scanner;
 // import org.junit.jupiter.api.Test;
 
 public class Main {
   public static void main(String[] args) {
-    for (int i = 1; i <= 5; i++) {
-      System.out.println("*".repeat(i));
+    System.out.println("Wpisz podstawę trójkąta: ");
+    Scanner scanner = new Scanner(System.in);
+
+    int a = scanner.nextInt();
+    
+    for (int i = 1; i <= a; i++){
+      if (i == a){
+        System.out.println("*".repeat(i));
+      } else if (i == a - (a-1) || i == a - a - (a-2)){
+        System.out.println("*".repeat(i));
+      } else {
+        System.out.println("*".repeat(1) + " ".repeat(i-2) + "*".repeat(1));
+      }
     }
   }
 
