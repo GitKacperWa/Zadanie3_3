dd18258b7803ad04fa5d7f3177a67987406ab4f5
GitKacperWa <kwalas@onet.pl>
2025-11-28 23:20:44 +0000

    Finalna_wersja

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,23 @@
+
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Scanner;
 // import org.junit.jupiter.api.Test;
 
 public class Main {
   public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-    
-      int a = scanner.nextInt();
-     
-      for (int i = 1; i <= 5; i++){
-        System.out.print(" ".repeat(a - i));
-        System.out.println("*".repeat(i));
-      }
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the number:");
+    int a = scanner.nextInt();
+
+    for (int i = 1; i <= a; i++) {
+      System.out.print(" ".repeat(a - i));
+      System.out.println("*".repeat(i));
     }
-  
+  }
 
   // @Test
   // void addition() {
-  //     assertEquals(2, 1 + 1);
+  // assertEquals(2, 1 + 1);
   // }
 }
